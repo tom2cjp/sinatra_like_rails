1c59fc2c7eeafd9e8a9912a662074f0749f35e64
tom2cjp <tom.cjp.cn@gmail.com>
2013-01-22 21:54:32 +0800

    add db config

--- app.rb
+++ app.rb
@@ -1,14 +1,13 @@
+# -*- encoding : utf-8 -*-
 require 'rubygems'
 require 'sinatra'
 require 'active_record'
 
-ActiveRecord::Base.establish_connection(
-  :adapter  => "mysql2",  
-  :host     => "localhost",  
-  :username => "root",  
-  :password => "db_pwd",  
-  :database => "ghost"  
-)
+set :root, File.dirname(__FILE__)
+set :app_file, __FILE__
+set :env, 'development'
+
+ActiveRecord::Base.establish_connection(YAML.load(File.read(File.join(settings.root, 'config/config.yml')))[settings.env])
 
 class Rec < ActiveRecord::Base
 end
